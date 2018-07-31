b8d9eff2e1721f7182be2ddda7a2cdd9c25a12c2
lpg <940942729@qq.com>
2018-07-31 23:56:08 +0800

    提交测试

---
+++ HelloWord/src/com/lpg/main/Main.java
@@ -0,0 +1,16 @@
+package com.lpg.main;
+
+/**
+ * 测试提交类
+ * @author lpg
+ * @date 2018年7月31日 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello word");
+		
+	}
+	
+}
