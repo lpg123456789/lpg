ea22467c6dda00b33221fc43025845edb450c82b
lpg <940942729@qq.com>
2018-08-04 15:15:03 +0800

    测试fast

---
+++ HelloWord/src/com/lpg/testFastLost/MyCollection.java
@@ -0,0 +1,47 @@
+package com.lpg.testFastLost;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyCollection {
+
+	public static void main(String[] args) {
+
+		final List<Integer> list = new ArrayList<>();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 1000; i++) {
+					System.out.println("增加数据 " + i);
+					list.add(i);
+				}
+			}
+		}).start();
+
+//		try {
+//			Thread.sleep(3);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (Integer i = 0; i < 1000; i++) {
+					System.out.println("移除数据 " + i);
+					list.remove(i);
+				}
+			}
+		}).start();
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("list信息是 " + list);
+	}
+}
