fe58b0e0b96b5beda3bbb2f3952f7f25c2c3ae4b
lpg <940942729@qq.com>
2018-08-13 14:27:32 +0800

    提交排序

--- HelloWord/src/com/lpg/sort/Task.java
+++ HelloWord/src/com/lpg/sort/Task.java
@@ -47,15 +47,37 @@ public class Task implements Comparable<Task> {
 		return "task [taskId=" + taskId + ", type=" + type + ", flag=" + flag + "]";
 	}
 
+	//这样容易搞复杂
 	//正数增序，负数将序
+//	@Override
+//	public int compareTo(Task task) {
+//		 // 如果number不等按number排序（增序
+//        if (this.getFlag() != task.getFlag()) {
+//            return this.getFlag() > task.getFlag() ? 1 : -1;
+//        } else {
+//        	return this.getType() > task.getType() ? 1:  -1;
+//        }
+//	}
+	
+	
+	/* 
+	 * 首先按照flag增序，然后按照type增序
+	 * (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	@Override
 	public int compareTo(Task task) {
-		 // 如果number不等按number排序（增序
-        if (this.getFlag() != task.getFlag()) {
-            return this.getFlag() > task.getFlag() ? 1 : -1;
-        } else {
-        	return this.getType() > task.getType() ? 1:  -1;
-        }
+		if(this.getFlag() > task.getFlag()) {
+			return 1;
+		}else if(this.getFlag() == task.getFlag()) {
+			if(this.getType() > task.getType()) {
+				return 1;
+			}else {
+				return -1;
+			}
+		}else {
+			return -1;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -63,11 +85,23 @@ public class Task implements Comparable<Task> {
 		Task task1=new Task(100, 4, 1);
 		Task task2=new Task(101, 1, 3);
 		Task task3=new Task(102, 2, 2);
-		Task task4=new Task(103, 3, 2);
+		Task task4=new Task(103, 4, 9);
+		Task task5=new Task(104, 1, 11);
+		Task task6=new Task(105, 5, 23);
+		Task task7=new Task(106, 8, 12);
+		Task task8=new Task(107, 9, 11);
+		Task task9=new Task(108, 10, 22);
+		Task task10=new Task(109, 11, 9);
 		taskList.add(task1);
 		taskList.add(task2);
 		taskList.add(task3);
 		taskList.add(task4);
+		taskList.add(task5);
+		taskList.add(task6);
+		taskList.add(task7);
+		taskList.add(task8);
+		taskList.add(task9);
+		taskList.add(task10);
 		Collections.sort(taskList);
 		
 		for (Task task : taskList) {
--- HelloWord/src/com/lpg/sort/Task2.java
+++ HelloWord/src/com/lpg/sort/Task2.java
@@ -49,27 +49,59 @@ public class Task2 {
 	}
 
 	public static void main(String[] args) {
-		List<Task2> taskList = new ArrayList<Task2>();
-		Task2 task1 = new Task2(100, 4, 1);
-		Task2 task2 = new Task2(101, 1, 3);
-		Task2 task3 = new Task2(102, 2, 2);
-		Task2 task4 = new Task2(103, 3, 2);
+		List<Task2> taskList=new ArrayList<>();
+		Task2 task1=new Task2(100, 4, 1);
+		Task2 task2=new Task2(101, 1, 3);
+		Task2 task3=new Task2(102, 2, 2);
+		Task2 task4=new Task2(103, 4, 9);
+		Task2 task5=new Task2(104, 1, 11);
+		Task2 task6=new Task2(105, 5, 23);
+		Task2 task7=new Task2(106, 8, 12);
+		Task2 task8=new Task2(107, 9, 11);
+		Task2 task9=new Task2(108, 10, 22);
+		Task2 task10=new Task2(109, 11, 9);
 		taskList.add(task1);
 		taskList.add(task2);
 		taskList.add(task3);
 		taskList.add(task4);
-
+		taskList.add(task5);
+		taskList.add(task6);
+		taskList.add(task7);
+		taskList.add(task8);
+		taskList.add(task9);
+		taskList.add(task10);
+//		这样容易搞复杂
+//		Collections.sort(taskList, new Comparator<Task2>() {
+//			@Override
+//			public int compare(Task2 o1, Task2 o2) {
+//				// 如果number不等按number排序（增序
+//				if (o1.getFlag() != o2.getFlag()) {
+//					return o1.getFlag() > o2.getFlag() ? 1 : -1;
+//				} else {
+//					return o1.getType() > o2.getType() ? 1 : -1;
+//				}
+//			}
+//		});
+		
+		
 		Collections.sort(taskList, new Comparator<Task2>() {
 			@Override
 			public int compare(Task2 o1, Task2 o2) {
 				// 如果number不等按number排序（增序
-				if (o1.getFlag() != o2.getFlag()) {
-					return o1.getFlag() > o2.getFlag() ? 1 : -1;
-				} else {
-					return o1.getType() > o2.getType() ? 1 : -1;
+				if(o1.getFlag() > o2.getFlag()) {
+					return 1;
+				}else if(o1.getFlag() == o2.getFlag()) {
+					if(o1.getType() > o2.getType()) {
+						return 1;
+					}else {
+						return -1;
+					}
+				}else {
+					return -1;
 				}
 			}
 		});
+		
 
 		for (Task2 task : taskList) {
 			System.out.println(task.toString());
