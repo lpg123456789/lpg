c5f9efe4bb17cb3746fec5f91dd5b50de4210543
940942729@qq.com <Administrator@lpg>
2018-08-15 22:44:10 +0800

    提交常量

---
+++ HelloWord/src/com/lpg/moudle/IProps/PropsConst.java
@@ -0,0 +1,93 @@
+package com.lpg.moudle.IProps;
+
+public class PropsConst {
+	/**
+	 * 装备
+	 */
+	public static final int TYPE_EQUIP = 1;
+	/**
+	 * 虚拟道具
+	 */
+	public static final int TYPE_VIRTUAL = 2;
+	/**
+	 * 功能物品
+	 */
+	public static final int TYPE_FUNCTION = 3;
+	/**
+	 * 消耗品
+	 */
+	public static final int TYPE_CONSUME = 4;
+	/**
+	 * 神魂
+	 */
+	public static final int TYPE_SOUL = 5;
+	/**
+	 * 符文
+	 */
+	public static final int TYPE_RUNE = 6;
+	/**
+	 * 徽章
+	 */
+	public static final int TYPE_BADGE = 7;
+	/**
+	 * 宠物
+	 */
+	public static final int TYPE_PET = 8;
+	/**
+	 * 战翼
+	 */
+	public static final int TYPE_ZY = 9;
+	/**
+	 * 英雄装备
+	 */
+	public static final int TYPE_EQUIPHERO = 10;
+	/**
+	 * 品质--白 0--对应的色值
+	 */
+	public static final String COLOR_WHITE = "A09F9C";
+	/**
+	 * 品质--绿 1--对应的色值
+	 */
+	public static final String COLOR_GREEN = "12DD12";
+	/**
+	 * 品质--紫 2--对应的色值
+	 */
+	public static final String COLOR_PURPLE = "D305FF";
+	/**
+	 * 品质--橙 3--对应的色值
+	 */
+	public static final String COLOR_ORANGE = "FFA300";
+	/**
+	 * 品质--红 4--对应的色值
+	 */
+	public static final String COLOR_RED = "FF0000";
+	/**
+	 * 品质--金 5--对应的色值
+	 */
+	public static final String COLOR_GOLD = "E1C21A";
+
+	/**
+	 * 颜色品质--白色
+	 */
+	public static final int QUALITY_WHITE = 0;
+	/**
+	 * 颜色品质--绿色
+	 */
+	public static final int QUALITY_GREEN = 1;
+	/**
+	 * 颜色品质--紫色
+	 */
+	public static final int QUALITY_PURPLE = 2;
+	/**
+	 * 颜色品质--橙色
+	 */
+	public static final int QUALITY_ORANGE = 3;
+	/**
+	 * 颜色品质--红色
+	 */
+	public static final int QUALITY_RED = 4;
+	/**
+	 * 颜色品质--金色
+	 */
+	public static final int QUALITY_GOLD = 5;
+}
