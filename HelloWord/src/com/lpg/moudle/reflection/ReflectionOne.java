2488ffd025ad9e62a5cc5d48e13cd255d5f88ad7
lpg <940942729@qq.com>
2018-08-27 19:09:52 +0800

    提交反射

---
+++ HelloWord/src/com/lpg/moudle/reflection/ReflectionOne.java
@@ -0,0 +1,40 @@
+package com.lpg.moudle.reflection;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionOne {
+
+	public static void main(String[] args) {
+		
+		gmOrder();
+	
+	}
+	
+	/**
+	 * 1.反射用全类名
+	 * 频繁创建了新对象cl.newInstance()
+	 */
+	public static void gmOrder() {
+		
+		String detail = "com.lpg.moudle.reflection.ReflectionOne_one_arg1";
+		String[] details = detail.split("_");
+		String className = details[0];
+		String method = details[1];
+		String arg1=details[2];
+		// 加载类
+		try {
+			Class<?> cl = Class.forName(className);
+			Object obj = cl.newInstance();
+			Method md = obj.getClass().getMethod(method, String.class);
+			md.invoke(obj, arg1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void one(String a) {
+		System.out.println(a);
+	}
+	
+}
